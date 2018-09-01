8e59d2ef3accbee6d801585aa6b76578f8541bff
umarcor <unai.martinezcorral@manchester.ac.uk>
2018-09-01 03:08:54 +0100

    downdload tarball, check sudo

--- build.sh
+++ build.sh
@@ -4,25 +4,29 @@
 dd bs=1G seek=2 of=rootfs.img count=0
 mkfs.ext4 rootfs.img
 mkdir mnt
-sudo mount -o loop rootfs.img ./mnt
+$(command -v sudo) mount -o loop rootfs.img ./mnt
 
 # Extract the rootfs archive
 cd mnt
-sudo tar xpf ../ArchLinuxARM-aarch64-latest.tar.gz
+tarball="ArchLinuxARM-aarch64-latest.tar.gz"
+if [ ! -f "../$tarball" ]; then
+  curl -L "http://os.archlinuxarm.org/os/$tarball" -o "../$tarball"
+fi
+$(command -v sudo) tar xpf "../$tarball"
 cd ..
 
-sudo mount -o bind /run/ mnt/run/
-sudo mount -o bind /dev/ mnt/dev/
-sudo mount -t proc none mnt/proc/
-sudo chroot mnt/ /usr/bin/pacman -Syu gcc ruby git make --noconfirm
-sudo chroot mnt/ /usr/bin/su alarm -c "cd /home/alarm && git clone https://github.com/beehive-lab/mambo.git && cd mambo && git submodule init && git submodule update"
-sudo umount mnt/run/
-sudo umount mnt/dev/
-sudo umount mnt/proc/
-sudo rm mnt/var/cache/pacman/pkg/*.tar.xz
+$(command -v sudo) mount -o bind /run/ mnt/run/
+$(command -v sudo) mount -o bind /dev/ mnt/dev/
+$(command -v sudo) mount -t proc none mnt/proc/
+$(command -v sudo) chroot mnt/ /usr/bin/pacman -Syu gcc ruby git make --noconfirm
+$(command -v sudo) chroot mnt/ /usr/bin/su alarm -c "cd /home/alarm && git clone https://github.com/beehive-lab/mambo.git && cd mambo && git submodule init && git submodule update"
+$(command -v sudo) umount mnt/run/
+$(command -v sudo) umount mnt/dev/
+$(command -v sudo) umount mnt/proc/
+$(command -v sudo) rm mnt/var/cache/pacman/pkg/*.tar.xz
 
 # Extract the kernel and initrd images
 cp mnt/boot/Image.gz .
 cp mnt/boot/initramfs-linux-fallback.img .
 
-sudo umount mnt
+$(command -v sudo) umount mnt
